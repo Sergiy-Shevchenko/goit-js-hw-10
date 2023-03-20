04738f996676110f67580f3b499bd3d122abad5a
Sergiy-Shevchenko <116056626+Sergiy-Shevchenko@users.noreply.github.com>
2023-03-20 21:18:51 +0000

    Deploying to gh-pages from @ Sergiy-Shevchenko/goit-js-hw-10@d02d720e8b43f0d7faa9e691c13ccacfd7df9b05 🚀

---
+++ index.53b2b4dd.js
@@ -0,0 +1,2 @@
+const o=fetch("https://restcountries.com/v3.1/all");console.log(o);
+//# sourceMappingURL=index.53b2b4dd.js.map
---
+++ index.5728c12a.js
@@ -0,0 +1,2 @@
+!function(){var o=fetch("https://restcountries.com/v3.1/all");console.log(o)}();
+//# sourceMappingURL=index.5728c12a.js.map
