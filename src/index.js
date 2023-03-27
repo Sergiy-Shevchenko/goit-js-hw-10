466f87893ef07746dfa5640512885c98aedfdd23
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-03-28 00:17:04 +0300

    finaly

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import './css/styles.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
-const DEBOUNCE_DELAY = 300;
 import {fetchCountries} from './fetchCountries'
 
-//--------------2-variant-----------------------------------
-
+const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info')  
@@ -32,15 +30,14 @@ return (countryList.innerHTML = ''), (countryInfo.innerHTML = '')
             countryInfo.insertAdjacentHTML('beforeend', makeCountryInfo (countries))  
             }
         else if (countries.length >= 10) {
-            addTextCountry ();
+            Notify.success("Too many matches found. Please enter a more specific name.");
         }
         else  {
         countryList.insertAdjacentHTML('beforeend', makeCountryList(countries));
          }
         })
-    .catch(changeTextCountry());
-    
-        
+    .catch(Notify.success("Too many matches found. Please enter a more specific name."));
+            
 }
 
 //-------------------card-list-country-------------
@@ -78,78 +75,9 @@ function makeCountryInfo (countries) {
         </div>
         <div class="flafs_name_info">
         <p class="info_title">Languages:</p>
-        <span class="info_list">${Object.values(languages)}</span>
+        <span class="info_list">${languages.map()}</span>
         </div>
         `
     }).join('');
     return makeCountry;
 };
-
-function addTextCountry () {
-    Notify.success("Too many matches found. Please enter a more specific name.");    
-}
-
-function changeTextCountry () {
-    //if (countries.length === 0) {
-     Notify.failure("Oops, there is no country with that name");
-    // return
-//}
-}
-
-
-//----------------1-variant----------
-// import NewsApiService from './fetchCountries.js'
-
-// const newsApiService = new NewsApiService();
-// const input = document.querySelector('#search-box');
-// const countryInfo = document.querySelector('.country-info');
-// const countryList = document.querySelector('.country-list');
-
-// //---------------input-form---------------
-
-// input.addEventListener('input', debounce(handleInputForm, 300))
-
-// function handleInputForm (e) {
-
-//     newsApiService.query = e.target.value.trim();
-//     newsApiService.fetchCountries().then(makeCountryList);
-    
-    
-//     // if (newsApiService.query.length !== 1) {
-//     //     newsApiService.fetchCountries().then(makeCountryList);
-//     // } else {newsApiService.fetchCountries().then(makeCountryInfo);
-//     // }
-// }
-
-// //----------------marcup-----------------------
-// function makeCountryInfo (data) {
-    
-// const countryInfoEl = data.map(({flags, name, capital, population, languages}) => {(data)
-//     return `
-//     <img class="flags_info" src="${flags.svg}" alt="">
-//     <h2>${name.official}</h2>
-//     <p>Capital:${capital}</p>
-//     <p>Population:${population}</p>
-//     <p>Languages:${Object.values(languages)}</p>
-//     `
-// }).join(' ');
-// console.log(data)
-// console.log(countryInfoEl);
-// countryInfo.insertAdjacentHTML('afterbegin', countryInfoEl) 
-// };
-// makeCountryInfo();
-
-// function makeCountryList (data) {
-    
-//     const countryListEl = data.map(({flags, name}) => {(data)
-//         return `
-//         <img class="flags_list" src="${flags.svg}" alt="">
-//         <h2>${name.official}</h2>
-        
-//         `
-//     }).join(' ');
-//     console.log(data)
-//     console.log(countryListEl);
-//     countryList.insertAdjacentHTML('afterbegin', countryListEl) 
-//     };
-//     makeCountryList();
